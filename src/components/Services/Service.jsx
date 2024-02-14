5bea48c82c4741a7ef2ca5e4de070c419cb62894
Terence Faid JABO <faidterence@outlook.com>
2024-02-14 09:52:48 +0200

    feat: Implement Service component with aligned sections

--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Service = () => {
   return (
-    <>
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
       <section>
         <div className="flex flex-col justify-center text-right capitalize max-w-[408px] text-sky-950 p-[40px]">
           <div className="flex gap-3 self-end text-2xl font-bold leading-10 whitespace-nowrap">
@@ -50,6 +50,53 @@ export const Service = () => {
           </p>
         </div>
       </section>
-    </>
+      <section>
+        <div className="flex flex-col justify-center text-right capitalize max-w-[408px] text-sky-950 p-[40px]">
+          <div className="flex gap-3 self-end text-2xl font-bold leading-10 whitespace-nowrap">
+            <p className="self-end mt-6">الخبرة والاعتماد</p>
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/5d29d594884dc02616ed8d76775c3d86df9b0d57a87a47576e04af1984aedbb2?apiKey=adb818a380d442cb937ab642e81f4d54&"
+              className="w-14 aspect-square"
+              alt=""
+            />
+          </div>
+          <p className="self-end mt-6">
+            نوفر مسارًا خاليًا من المتاعب لاستكشاف جمال عُمان ومن ثم الانتقال
+            بسلاسة إلى تأشيرات العمل
+          </p>
+        </div>
+        <div className="flex flex-col justify-center text-right capitalize max-w-[408px] text-sky-950 p-[40px]">
+          <div className="flex gap-3 self-end text-2xl font-bold leading-10 whitespace-nowrap">
+            <p className="self-end mt-6">الخبرة والاعتماد</p>
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/5d29d594884dc02616ed8d76775c3d86df9b0d57a87a47576e04af1984aedbb2?apiKey=adb818a380d442cb937ab642e81f4d54&"
+              className="w-14 aspect-square"
+              alt=""
+            />
+          </div>
+          <p className="self-end mt-6">
+            نوفر مسارًا خاليًا من المتاعب لاستكشاف جمال عُمان ومن ثم الانتقال
+            بسلاسة إلى تأشيرات العمل
+          </p>
+        </div>
+        <div className="flex flex-col justify-center text-right capitalize max-w-[408px] text-sky-950 p-[40px]">
+          <div className="flex gap-3 self-end text-2xl font-bold leading-10 whitespace-nowrap">
+            <p className="self-end mt-6">الخبرة والاعتماد</p>
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/5d29d594884dc02616ed8d76775c3d86df9b0d57a87a47576e04af1984aedbb2?apiKey=adb818a380d442cb937ab642e81f4d54&"
+              className="w-14 aspect-square"
+              alt=""
+            />
+          </div>
+          <p className="self-end mt-6">
+            نوفر مسارًا خاليًا من المتاعب لاستكشاف جمال عُمان ومن ثم الانتقال
+            بسلاسة إلى تأشيرات العمل
+          </p>
+        </div>
+      </section>
+    </div>
   );
 };
